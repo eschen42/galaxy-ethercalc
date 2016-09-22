a3c72ae3e4e23e82be30858b2457c0f2a3a6f12f
Art Eschenlauer <eschen@alumni.princeton.edu>
2016-09-22 10:13:40 -0500

    demonstration of a potential configuration

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # galaxy-ethercalc
+- `DockerFile` - This file defines the build for the `eschen42/ethercalc` docker image.
+- `etc-init.d-galaxy_docker` - This file is to be copied to `/etc/init.d/galaxy_docker` for use in starting and stopping `galaxy-compose.yml` as a service.
+- `galaxy-compose.yml` - Start or stop Galaxy, ethercalc, and redis containers.
+- `start.sh` - Start `galaxy-compose.yml`; must be run as root.
+- `stop.sh` -  Stop  `galaxy-compose.yml`; must be run as root.
---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# this file is to be run as root or sudo
+
+# pushd to the directory where this script and galaxy-compose.yml both reside
+#   ref: https://stackoverflow.com/questions/242538/unix-shell-script-find-out-which-directory-the-script-file-resides#comment15303203_1638397
+pushd "$(dirname "$(readlink "$BASH_SOURCE" || echo "$BASH_SOURCE")")"
+
+# you may want to adjust this to taste; this user *MUST* exist in /etc/passwd
+DOCKER_USER=galaxy
+
+# extract and export UID for docker user
+DOCKER_USER=$(grep "^$DOCKER_USER:" /etc/passwd | cut -f 3 -d ':') ; export DOCKER_USER
+
+echo running docker-compose up for UID=$DOCKER_USER
+docker-compose -f galaxy-compose.yml up -d
+
